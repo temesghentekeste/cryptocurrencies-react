fdb4d9c6021b2e7e6eb11e2f4734768980054c00
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-06-13 12:28:20 +0300

    Add axios, RTK, react-redux npm packages; add APIEndPoints.js; fetch data and display number of crypto coins fetched

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
 
-function App() {
+import { getCryptocurrenciesAsync } from './redux/cryptocurrenciesSlice';
+
+const App = () => {
+  const dispatch = useDispatch();
+  const { loading, cryptoCurrencies } = useSelector(
+    (state) => state.cryptocurrencies,
+  );
+
+  useEffect(() => {
+    dispatch(getCryptocurrenciesAsync());
+  }, [dispatch]);
+
+  if (loading || loading === null || loading === undefined) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <div className="App">
-      <h2>Cryptocurrencies catalog</h2>
+      <h2>
+        Cryptocurrencies catalog:
+        {' '}
+        {cryptoCurrencies.length}
+        {' '}
+        crypto coins
+        fetched!
+      </h2>
     </div>
   );
-}
+};
 
 export default App;
