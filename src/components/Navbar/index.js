87a5b386600671358656e84acecfe1e032a6d331
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-06-13 13:16:46 +0300

    Update Navbar; add basic link  and form for searching crpyptocurrencies

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,28 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => <header>Navbar</header>;
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+  console.log('Navbar...');
+  return (
+    <header className={styles.header} data-testid="header">
+      <div className={styles.header__right}>
+        <Link to="/">
+          <h1>Cryptocurrencies</h1>
+        </Link>
+      </div>
+      <div className={styles.header__search}>
+        <input
+          type="text"
+          name="keyword"
+          placeholder="Search for something. Ex. iExec, Bitcoin, DOGEUSD..."
+        />
+        <form>
+          <input type="submit" value="Search" className={styles.btnSubmit} />
+        </form>
+      </div>
+    </header>
+  );
+};
 
 export default Navbar;
