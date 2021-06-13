03c73175ebcef100cbeb76d6bf9add72a1cebd0a
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-06-13 16:10:09 +0300

    Rename CryptoQuote container component

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import CryptoQuote from '../containers/CryptoQuote/CryptoQuote';
+import CryptoQuote from '../containers/CryptoQuote';
 import App from './App';
 import Navbar from './Navbar';
 
